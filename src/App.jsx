0966240f4ca02ad0636f5133cfd39516b5fba620
Mariobuddy <javascript97559755@gmail.com>
2024-06-02 20:47:00 +0530

    #app_design

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
+import styled from 'styled-components'
+import { ToastContainer } from 'react-toastify'
+import "react-toastify/dist/ReactToastify.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 
 const App = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkScreenWidth = () => {
+      setIsMobile(window.innerWidth <= 768); // Adjust the threshold as needed
+    };
+
+    checkScreenWidth();
+
+    window.addEventListener("resize", checkScreenWidth);
+
+    return () => {
+      window.removeEventListener("resize", checkScreenWidth);
+    };
+  }, []);
   return (
-    <div>App</div>
+   <BrowserRouter>
+    <Wrapper>
+    <ToastContainer
+            position={isMobile?"top-center":"bottom-right"}
+           style={{zIndex:"2147483647"}}
+          />
+      </Wrapper>
+   </BrowserRouter>
   )
 }
 
 export default App
+
+
+const Wrapper=styled.div``
---
+++ src/GlobalStyle.js
@@ -0,0 +1,32 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+
+:root{
+ 
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Roboto', sans-serif;
+  text-decoration: none;
+
+/* 
+font-family: 'Alkatra', cursi
+ve;
+font-family: 'Allura', cursive;
+font-family: 'Clicker Script', cursive;
+font-family: 'Great Vibes', cursive;
+font-family: 'Lato', sans-serif;
+font-family: 'Poppins', sans-serif;
+font-family: 'Roboto', sans-serif;
+font-family: 'Rouge Script', cursive; */
+}
+
+html {
+  font-size: 62.5%;
+}
+
+`;
